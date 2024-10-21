7d3f161045832f570aa7aad3262017f07e605b55
Tim <twoo5688@gmail.com>
2024-10-21 13:18:54 -0500

    added wishlist/wishlistitem/added to user service

---
+++ ShopForHome_Users/ShopForHome_Users/src/main/java/com/cogent/ShopForHome_Users/enums/Role.java
@@ -0,0 +1,5 @@
+package com.cogent.ShopForHome_Users.enums;
+
+public enum Role {
+	ADMIN, USER
+}
--- ShopForHome_Users/ShopForHome_Users/src/main/java/com/cogent/ShopForHome_Users/service/UserService.java
+++ ShopForHome_Users/ShopForHome_Users/src/main/java/com/cogent/ShopForHome_Users/service/UserService.java
@@ -1,6 +1,8 @@
 package com.cogent.ShopForHome_Users.service;
 
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,8 +15,11 @@ public class UserService {
 	@Autowired
 	UserRepository userRepository;
 	
-	public void saveUser(User user) {
-        userRepository.save(user);
+	public User saveUser(User user) {
+        return userRepository.save(user);
     }
     
+	public Optional<User> findUserById(int userId) {
+		return userRepository.findById(userId);
+	}
 }
