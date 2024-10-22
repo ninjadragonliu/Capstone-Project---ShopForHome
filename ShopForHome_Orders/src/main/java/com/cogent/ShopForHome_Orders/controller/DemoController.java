882cecfd82323c616d7742c84d5e674228b93133
ninjadragonliu <142239451+ninjadragonliu@users.noreply.github.com>
2024-10-22 09:22:29 -0400

    added methods to order service, added crud methods updated table name

--- ShopForHome_Orders/src/main/java/com/cogent/ShopForHome_Orders/controller/DemoController.java
+++ ShopForHome_Orders/src/main/java/com/cogent/ShopForHome_Orders/controller/DemoController.java
@@ -1,9 +1,20 @@
 package com.cogent.ShopForHome_Orders.controller;
 
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.cogent.ShopForHome_Orders.model.Order;
 import com.cogent.ShopForHome_Orders.service.OrderItemService;
 import com.cogent.ShopForHome_Orders.service.OrderService;
 
@@ -16,9 +27,53 @@ public class DemoController {
 	OrderItemService orderItemService;
 
 
-	@GetMapping(value="/hi")
-	public String hi(){
-		return "hi.";
+	@PostMapping("/register")
+	public ResponseEntity<Order> register(@RequestBody Order order) {
+		Optional<Order> existingOrder = orderService.findOrderById(order.getOrderId());
+		if (!existingOrder.isEmpty()) {
+			return ResponseEntity.badRequest().build();
+		}
+		orderService.saveOrder(order);
+		return ResponseEntity.ok(order);
+	}
+
+	@GetMapping("/orders")
+	public ResponseEntity<List<Order>> getOrders() {
+		List<Order> orderList = orderService.getAllOrders();
+		if (orderList.isEmpty()) {
+			return ResponseEntity.noContent().build();
+		}
+		return ResponseEntity.ok(orderList);
+	}
+
+
+	@GetMapping("/orders/{orderId}")
+	public ResponseEntity<Order> getOrderById(@PathVariable int orderId) {
+		Optional<Order> existingOrder = orderService.findOrderById(orderId);
+		if (existingOrder.isPresent()) {
+			return ResponseEntity.ok(existingOrder.get());
+		}
+		return ResponseEntity.notFound().build();
+	}
+
+	@PutMapping("/orders/{orderId}")
+	public ResponseEntity<Order> updateOrder(@PathVariable int orderId, @RequestBody Order order) {
+		Optional<Order> existingOrder = orderService.findOrderById(orderId);
+		if (existingOrder.isEmpty()) {
+			return ResponseEntity.notFound().build();
+		}
+		orderService.updateOrder(orderId, order);
+		return ResponseEntity.ok(order);
+	}
+
+	@DeleteMapping("/orders/{orderId}")
+	public ResponseEntity<String> deleteOrder(@PathVariable int orderId) {
+		Optional<Order> existingOrder = orderService.findOrderById(orderId);
+		if (existingOrder.isEmpty()) {
+			return ResponseEntity.notFound().build();
+		}
+		orderService.deleteOrder(orderId);
+		return ResponseEntity.ok("Order deleted successfully");
 	}
 
 }
--- ShopForHome_Orders/src/main/java/com/cogent/ShopForHome_Orders/service/OrderService.java
+++ ShopForHome_Orders/src/main/java/com/cogent/ShopForHome_Orders/service/OrderService.java
@@ -1,18 +1,48 @@
 package com.cogent.ShopForHome_Orders.service;
 
-
+import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.cogent.ShopForHome_Orders.model.Order;
 import com.cogent.ShopForHome_Orders.respository.OrderRepository;
 
-
-
-
 @Service
-public class OrderService{
+public class OrderService {
 	@Autowired
 	OrderRepository orderRepository;
-	
-	
+
+	public Order saveOrder(Order order) {
+		return orderRepository.save(order);
+	}
+
+	public List<Order> getAllOrders() {
+		return orderRepository.findAll();
+	}
+
+	public Optional<Order> findOrderById(int orderId) {
+		return orderRepository.findById(orderId);
+	}
+
+	public boolean deleteOrder(int orderId) {
+		Optional<Order> existingOrder = orderRepository.findById(orderId);
+		if (existingOrder.isEmpty()) {
+			return false;
+		} else {
+			orderRepository.deleteById(orderId);
+			return true;
+		}
+	}
+
+	public Order updateOrder(int orderId, Order order) {
+		Optional<Order> existingOrder = orderRepository.findById(order.getOrderId());
+		if (existingOrder.isEmpty()) {
+			return null;
+		}
+		order.setOrderId(orderId);
+		return orderRepository.saveAndFlush(order);
+	}
+
 }
