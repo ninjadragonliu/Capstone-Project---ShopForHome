35e2b8905ecc13a90146f7b1d0b16b28bc59b233
IA <isabellastor25@gmail.com>
2024-10-22 12:18:37 -0400

    discovery server name change

--- ShopForHome_DiscoveryServer/src/main/java/com/example/ServiceRegistry/DiscoveryServerApplication.java
+++ ShopForHome_DiscoveryServer/src/main/java/com/example/ServiceRegistry/DiscoveryServerApplication.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class DiscoveryClientApplication {
+public class DiscoveryServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DiscoveryClientApplication.class, args);
+		SpringApplication.run(DiscoveryServerApplication.class, args);
 	}
 
 }
--- ShopForHome_DiscoveryServer/src/main/java/com/example/ServiceRegistry/ServletInitializer.java
+++ ShopForHome_DiscoveryServer/src/main/java/com/example/ServiceRegistry/ServletInitializer.java
@@ -7,7 +7,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(DiscoveryClientApplication.class);
+		return application.sources(DiscoveryServerApplication.class);
 	}
 
 }
