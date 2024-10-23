6015fa795b2447a9b0553e9931f028b43b1f9588
IA <isabellastor25@gmail.com>
2024-10-23 11:35:57 -0400

    code clean up

--- ShopForHome_Users/src/main/java/com/cogent/ShopForHome_Users/objectreference/Wishlist.java
+++ ShopForHome_Users/src/main/java/com/cogent/ShopForHome_Users/objectreference/Wishlist.java
@@ -4,15 +4,6 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.hibernate.annotations.CreationTimestamp;
-
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.Table;
 
 
 public class Wishlist {
--- ShopForHome_Users/src/main/java/com/cogent/ShopForHome_Users/objectreference/WishlistItem.java
+++ ShopForHome_Users/src/main/java/com/cogent/ShopForHome_Users/objectreference/WishlistItem.java
@@ -2,16 +2,6 @@ package com.cogent.ShopForHome_Users.objectreference;
 
 import java.time.LocalDateTime;
 
-import org.hibernate.annotations.CreationTimestamp;
-
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
-
 
 public class WishlistItem {
 
