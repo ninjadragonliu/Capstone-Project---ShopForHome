9d697a91666ec4e462ba510622f7c923adc7ecc5
IA <isabellastor25@gmail.com>
2024-10-23 12:37:14 -0400

    route changes

---
+++ frontend/shopforhome/src/app/user-interface-header/user-interface-header.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { UserInterfaceHeaderComponent } from './user-interface-header.component';
+
+// describe('UserInterfaceHeaderComponent', () => {
+//   let component: UserInterfaceComponent;
+//   let fixture: ComponentFixture<UserInterfaceComponent>;
+
+//   beforeEach(async () => {
+//     await TestBed.configureTestingModule({
+//       imports: [UserInterfaceComponent]
+//     })
+//     .compileComponents();
+
+//     fixture = TestBed.createComponent(UserInterfaceComponent);
+//     component = fixture.componentInstance;
+//     fixture.detectChanges();
+//   });
+
+//   it('should create', () => {
+//     expect(component).toBeTruthy();
+//   });
+// });
