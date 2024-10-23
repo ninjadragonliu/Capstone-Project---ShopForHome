cd56d6416110d64f0743989d77649d851b320702
ninjadragonliu <142239451+ninjadragonliu@users.noreply.github.com>
2024-10-23 14:53:48 -0400

    adjusted navbar and added carousel for pictures

--- frontend/shopforhome/src/app/app.module.ts
+++ frontend/shopforhome/src/app/app.module.ts
@@ -1,10 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
-
-
 import { AppComponent } from './app.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgbCarousel, NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { OutdoorsComponent } from './outdoors/outdoors.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HomeComponent } from './home/home.component';
@@ -12,6 +9,7 @@ import { SignupComponent } from './signup/signup.component';
 import { AdministratorInterfaceComponent } from './administrator-interface/administrator-interface.component';
 import { UserInterfaceHeaderComponent } from './user-interface-header/user-interface-header.component';
 import { LoginComponent } from './login/login.component';
+import { CarouselModule } from 'ngx-bootstrap/carousel';
 
 
 @NgModule({
@@ -22,7 +20,8 @@ import { LoginComponent } from './login/login.component';
   imports: [
     BrowserModule,
     NgbModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CarouselModule
   ],
   providers: [],
   bootstrap: [AppComponent]
