a2305b862e96753fdaf9a15d5e7e7cf92396e86f
ninjadragonliu <142239451+ninjadragonliu@users.noreply.github.com>
2024-10-23 11:35:03 -0400

    created form in signup component

--- frontend/shopforhome/src/app/app-routing.module.ts
+++ frontend/shopforhome/src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import { RouterModule } from '@angular/router';
 import { OutdoorsComponent } from './outdoors/outdoors.component';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { SignupComponent } from './signup/signup.component';
 
 
 const routes = [
   { path: 'home', component: HomeComponent },
   { path: 'outdoors', component: OutdoorsComponent },
+  { path: 'signup', component: SignupComponent }
 ]
 @NgModule({
   declarations: [],
