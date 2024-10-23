e842d81e9ce50e7763f1237d9f0c29f01f4e31e4
twoo5688 <61475537+twoo5688@users.noreply.github.com>
2024-10-23 11:44:22 -0500

    added itemrequest and finished user/product services. still working on cart services

---
+++ ShopForHome_Carts/src/main/java/com/cogent/ShopForHome_Carts/objectreference/ItemRequest.java
@@ -0,0 +1,19 @@
+package com.cogent.ShopForHome_Carts.objectreference;
+
+public class ItemRequest {
+	int productId;
+	int quantity;
+	
+	public int getProductId() {
+		return productId;
+	}
+	public void setProductId(int productId) {
+		this.productId = productId;
+	}
+	public int getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+}
