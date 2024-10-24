8578af59318af0f86df6d550d4a131776dcff5cf
twoo5688 <61475537+twoo5688@users.noreply.github.com>
2024-10-24 16:38:42 -0500

    fixed add to wishlist parameter

---
+++ ShopForHome_Carts/src/main/java/com/cogent/ShopForHome_Carts/objectreference/ItemRequestWishlist.java
@@ -0,0 +1,12 @@
+package com.cogent.ShopForHome_Carts.objectreference;
+
+public class ItemRequestWishlist {
+	int productId;
+	
+	public int getProductId() {
+		return productId;
+	}
+	public void setProductId(int productId) {
+		this.productId = productId;
+	}
+}
