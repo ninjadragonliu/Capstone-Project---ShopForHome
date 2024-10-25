666fafa107ccaf3600cf17278948ef61c4033e64
IA <isabellastor25@gmail.com>
2024-10-25 01:21:25 -0400

    seperate counter for adding items

--- frontend/shopforhome/src/app/outdoors/outdoors.component.ts
+++ frontend/shopforhome/src/app/outdoors/outdoors.component.ts
@@ -13,28 +13,34 @@ import { ChildActivationStart } from '@angular/router';
   templateUrl: './outdoors.component.html',
   styleUrl: './outdoors.component.css'
 })
+
+
+
 export class OutdoorsComponent implements OnInit {
 
   counter:any = 1;
 
-
-  increment(){
-    this.counter++;
-  }
-  decrement(){
-    if(this.counter <=1) return;
-    this.counter--;
-  }
+  products:any | undefined;
+ 
 
   constructor(private productService: ProductService, private cartService:CartService) { }
 
-    products:any | undefined;
+    
 
-    onClick(productId:any){
 
-      const item = this.cartService.addProductToCart(currentUser.userId, productId, this.counter);
-      item.subscribe(data=> {
-         console.log(data);
+    increment(productAccess:number){
+      this.products[productAccess].counter++;
+    }
+    decrement(productAccess:number){
+      if(this.products[productAccess].counter <=1) return;
+      this.products[productAccess].counter--;
+    }
+
+    onClick(productId:any, productAccess:number){
+
+      const item = this.cartService.addProductToCart(currentUser.userId, productId, this.products[productAccess].counter);
+      item.subscribe( data => {
+        console.log(data);
       })
     };
     
@@ -44,10 +50,21 @@ export class OutdoorsComponent implements OnInit {
 
       // trying to have this autopopulate before onClick needs work, idk if this works
       // disregard my change to a promise, this needs to be worked out
-      await this.productService.getProductsByCategory("outdoors").then((data) => {
-         data.subscribe((response: any) => {
-              this.products = response;
-         });
+      await this.productService.getProductsByCategory("outdoors").then((products) => {
+         products.subscribe(productList => {
+
+
+           productList.forEach((element:any) => {
+           
+            element["counter"] = 1;
+
+            
+ 
+          });
+
+          this.products = productList;
+
+       });
       });
      
       
